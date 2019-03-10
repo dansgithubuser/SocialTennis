e46ae7f1d6d5d736e8b28aa86edc4393d9dc8eba
rhomboyds <rhomboyds@gmail.com>
2019-03-09 19:35:37 -0500

    Create Login page; fix login redirect

--- SocialTennis/urls.py
+++ SocialTennis/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('signup', views.signup),
     path('home', views.home),
+    path('login', auth_views.LoginView.as_view(template_name='login.html', redirect_field_name='home')),
 ]
