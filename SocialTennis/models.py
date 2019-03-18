9934d2e5d0e0a74c98d802635b592867bf281400
rhomboyds <rhomboyds@gmail.com>
2019-03-17 21:18:57 -0400

    Create model for Friend and Events

--- SocialTennis/models.py
+++ SocialTennis/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Friend(models.Model):
+    user = models.ForeignKey(User, models.CASCADE)
+    name = models.TextField()
+
+class Event(models.Model):
+    friend = models.ForeignKey('Friend', models.CASCADE)
+    server = models.TextField(choices=[('user', 'user'), ('friend', 'friend'), ('neither', 'neither')])
