bbf7075b0f9164e9699491f156a561c69734d04b
rhomboyds <rhomboyds@gmail.com>
2019-04-06 20:37:55 -0400

    Timestamps added for each event

--- SocialTennis/models.py
+++ SocialTennis/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 class Friend(models.Model):
     user = models.ForeignKey(User, models.CASCADE)
@@ -8,3 +9,4 @@ class Friend(models.Model):
 class Event(models.Model):
     friend = models.ForeignKey('Friend', models.CASCADE)
     server = models.TextField(choices=[('user', 'user'), ('friend', 'friend'), ('neither', 'neither')])
+    created_at = models.DateTimeField(auto_now_add=True)
